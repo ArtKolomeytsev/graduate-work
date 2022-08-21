0e083db6ee1cef5cafa4d42f4b8549938773297d
DrRedyx <88320874+DrRedyx@users.noreply.github.com>
2022-08-22 01:51:31 +0300

    Первоначальный User, после добавления полного security нужно будет поменять логику сохранения пароля и сохранения пользователя

--- src/main/java/ru/skypro/homework/controller/UserController.java
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -1,7 +1,8 @@
 package ru.skypro.homework.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.skypro.homework.dto.RegisterReq;
+import ru.skypro.homework.dto.*;
 import ru.skypro.homework.entities.User;
 import ru.skypro.homework.mapper.UserMapper;
 import ru.skypro.homework.service.impl.UserServiceImpl;
@@ -17,8 +18,24 @@ public class UserController {
     }
 
     @PostMapping(path = "/add")
-    public User add(@RequestBody  RegisterReq registerReq) {
-        return null;
+    public ResponseEntity<UsersDto> add(@RequestBody  CreateUser createUser, @RequestBody RegReg regReg) {
+        UsersDto usersDto = userService.add(createUser, regReg);
+        return ResponseEntity.ok(usersDto);
+    }
+
+    @GetMapping(path = "/me")
+    public ResponseEntity<ResponseWrapperUser> getUsers() {
+        return ResponseEntity.ok(userService.findAllUsers());
+    }
+
+    @PatchMapping(path = "/me")
+    public ResponseEntity<UsersDto> updateUser(UsersDto usersDto) {
+        return ResponseEntity.ok(userService.updateUser(usersDto));
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<UsersDto> getUser(@PathVariable("id") int id) {
+        return ResponseEntity.ok(userService.getUser(id));
     }
 
 }
--- src/main/java/ru/skypro/homework/dto/RegReg.java
+++ src/main/java/ru/skypro/homework/dto/RegReg.java
@@ -16,5 +16,4 @@ public class RegReg {
         }
 
     private RoleEnum roleEnum;
-    private String username;
 }
